2e8a20d71526e1fa4e8213e37a233fca1dacc09e
JB Braendel <Jbud@live.ca>
2018-03-30 10:41:04 -0400

    WIP: Sync

--- js/scripts/character.ai.combat.melee.js
+++ js/scripts/character.ai.combat.melee.js
@@ -27,6 +27,33 @@ define(
                     _script = this;
                 },
 
+                calculateDamage: (target) => {
+
+                    let wpnDamage = 1; // FIXME: Check actual weapon; otherwise default is hand-to-hand for 1?
+                    let strContribution = 0.4;
+                    let str = character.stats.str.cur;
+                    let maxCrit = 1.6, minCrit = 0.6;
+                    let dex = character.stats.dex.cur;
+                    let wpnLevel = 1; // FIXME: Check actual weapon; otherwise default is 1?
+                    let maxDex = 2.0 * wpnLevel,
+                        minDex = 0.5 * wpnLevel,
+                        dexScore = (dex - minDex) / (maxDex - minDex);
+                    let critScore = Math.min(1.0, Math.max(0.0, dexScore)); // [0,1], 1 if dex = 2*wpnLevel, 0 if dex = 0.5*wpnLevel
+                    minCrit += critScore * 0.4; // Shift where our crit is based off of critScore
+                    maxCrit += critScore * 0.4;
+                    let rng = Math.random();
+                    let crit = minCrit + (maxCrit - minCrit) * rng;
+                    let damage = wpnDamage * (1.0 + strContribution * str) * crit;
+
+
+                    // Resist
+                    let AC = 2.0; // FIXME: Fetch from character armour
+                    let hit = damage * (damage) / (AC + damage);
+
+                    hit = Math.round(hit);
+                    return hit;
+                },
+
                 attackTarget: (target) => {
                     assert(target instanceof Character, "Target is not a character..");
 
@@ -35,10 +62,10 @@ define(
                     }
 
                     // FIXME: Abstract damage
-                    const DAMAGE = 10;
-                    this.Log(` I TOTALLY SCRATCHED YOU FOR ${DAMAGE}`, LOG_DEBUG);
+                    const damage = this.calculateDamage(target);
+                    this.Log(` I TOTALLY SCRATCHED YOU FOR ${damage}`, LOG_DEBUG);
 
-                    target.damage(DAMAGE, character, {});
+                    target.damage(damage, character, {});
 
                     return true;
                 }
